c81196b72c7147e8673c70c27823be1141f7136b
betoinu <josuayerbe@idarte.eus>
2025-11-19 19:40:35 +0100

    Update suggestion-generator.js

--- components/suggestion-generator.js
+++ components/suggestion-generator.js
@@ -1,7 +1,7 @@
 import { GroqAPIService } from '../services/api-service.js';
 import { PromptBuilder } from '../utils/prompt-builder.js';
 import { ResultsDisplay } from './results-display.js';
-import { setLoadingState, showError, hideError } from '../main.js';
+import { setLoadingState, showError, hideError } from '../js/main.js';
 
 export async function generateSuggestions(data) {
     setLoadingState(true);
@@ -25,4 +25,5 @@ export async function generateSuggestions(data) {
     } finally {
         setLoadingState(false);
     }
+
 }
