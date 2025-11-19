67358f04b25ef4217a0889843049dc9b32bbc6ba
betoinu <josuayerbe@idarte.eus>
2025-11-19 16:23:11 +0100

    Update data-service.js

--- src/services/data-service.js
+++ src/services/data-service.js
@@ -3,7 +3,7 @@ export class CurriculumDataService {
 
     static async loadData() {
         try {
-            const response = await fetch('./json/curriculumBD.json');
+            const response = await fetch('./public/json/curriculumBD.json');
             if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
             
             this.curriculumData = await response.json();
@@ -36,3 +36,4 @@ export class CurriculumDataService {
         return null;
     }
 }
+
