e86b448991ee9e0acd5066206a1a746c81c3d608
betoinu <josuayerbe@idarte.eus>
2025-11-19 15:28:06 +0100

    Add files via upload

---
+++ src/config/app-config.js
@@ -0,0 +1,7 @@
+export const APP_CONFIG = {
+    API: {
+        GROQ_API_KEY: "", // Erabiltzaileak sartuko du
+        BASE_URL: "https://api.groq.com/openai/v1/chat/completions",
+        MODEL: "llama-3.1-8b-instant"
+    }
+};
