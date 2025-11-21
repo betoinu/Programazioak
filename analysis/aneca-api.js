72d965d7f51baa17371e088b6aab92fc0bb79bfb
betoinu <josuayerbe@idarte.eus>
2025-11-21 19:58:33 +0100

    Update aneca-api.js

--- analysis/aneca-api.js
+++ analysis/aneca-api.js
@@ -175,10 +175,12 @@ class AnecaAPI {
         
         return competencias;
     }
+}    
 // ✅ Exportar al ámbito global para scripts no módulo
 if (typeof window !== 'undefined') {
     window.AnecaAPI = AnecaAPI;
 }
-}
+
+
 
 
