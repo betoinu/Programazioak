6d069ea72339e463ec35e1b1671e214c700d35cb
betoinu <josuayerbe@idarte.eus>
2025-11-21 20:43:41 +0100

    Update competence-mapper.js

--- analysis/competence-mapper.js
+++ analysis/competence-mapper.js
@@ -1,5 +1,5 @@
 // 📁 analysis/competence-mapper.js - ACTUALIZADO
-class CompetenceMapper {
+export class CompetenceMapper {
     static async generarMatrizCompetenciasRA(curriculumData) {
         console.log("🔄 Generando matriz de competencias desde API...");
         
@@ -138,5 +138,6 @@ class CompetenceMapper {
         };
     }
 }
+
 
 
